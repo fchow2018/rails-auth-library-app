77f9347c903f24d85644c4485f0db334af74d290
juancarlucci <juancarloscollins@gmail.com>
2018-02-25 11:20:38 -0800

    update: user login and logout working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to root_path
+    login(@user)
+    flash[:notice] = "User created"
+    redirect_to @user
   end
 
   def show
     @user = User.find_by_id(params[:id])
   end
+
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  #http://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
   has_secure_password
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+    
+  end
+
 end
